38f7a4a3e8d90751fb133c6913182aaa775b8ec8
starybog <nikitapishchalin@gmail.com>
2024-11-11 17:36:07 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-filename = "text.txt"
-with open(filename, "r", encoding="utf-8") as file:
-   text = file.read()
-   words = text.split()
-   word_count = len(words)
-   print(word_count)
+filename = "text.txt" #cоздается переменная filename, которая хранит имя файла text.txt. 
+with open(filename, "r", encoding="utf-8") as file: #открывает файл "text.txt" для чтения (r) в кодировке UTF-8. 
+   text = file.read() #Считывает все содержимое файла в переменную text. 
+   words = text.split() #Разбивает строку text на список слов
+   word_count = len(words) #Подсчитывает количество слов в списке words и сохраняет результат в переменную word_count. 
+   print(word_count) #Выводит количество слов word_count
 
